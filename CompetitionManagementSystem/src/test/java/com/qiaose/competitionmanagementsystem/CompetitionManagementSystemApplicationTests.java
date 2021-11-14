c21cb5d0af277c7dc0058527453470429f836ed1
qiaosefennv <791917396@qq.com>
2021-11-14 10:11:46 +0800

    初始项目

---
+++ CompetitionManagementSystem/src/test/java/com/qiaose/competitionmanagementsystem/CompetitionManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qiaose.competitionmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompetitionManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
