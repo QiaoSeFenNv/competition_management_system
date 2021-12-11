ab8dc11e60e881715345faddb0056ca9ce56fe82
qiaosefennv <791917396@qq.com>
2021-12-11 20:34:25 +0800

    pom 干净

--- CompetitionManagementSystem/src/test/java/com/qiaose/competitionmanagementsystem/CompetitionManagementSystemApplicationTests.java
+++ CompetitionManagementSystem/src/test/java/com/qiaose/competitionmanagementsystem/CompetitionManagementSystemApplicationTests.java
@@ -1,6 +1,6 @@
 package com.qiaose.competitionmanagementsystem;
 
-import jdk.nashorn.internal.ir.annotations.Ignore;
+
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
