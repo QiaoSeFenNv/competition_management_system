91ad91668a302f87a40f3ec651084c12defff7d6
qiaosefennv <791917396@qq.com>
2021-12-11 20:24:00 +0800

    pom 又烂掉了

--- CompetitionManagementSystem/src/test/java/com/qiaose/competitionmanagementsystem/CompetitionManagementSystemApplicationTests.java
+++ CompetitionManagementSystem/src/test/java/com/qiaose/competitionmanagementsystem/CompetitionManagementSystemApplicationTests.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @Slf4j
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CompetitionManagementSystemApplicationTests {
 
 
